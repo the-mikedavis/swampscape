d4a1fc9579120d677f576878127be0b1204ffbbb
Michael Davis <mcd164@miami.edu>
2018-04-18 16:16:16 -0400

    Add structure for the About page

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.get('/', function(req, res) {
   res.render("landing.html");
 });
 
+//-----------TODO: revise for new guides layout---------------------
 app.get('/guides', function(req, res) {
   res.render('guides.html', sheets.cache);
 });
@@ -34,9 +35,14 @@ app.get('/guides/:guide', function(req, res) {
   else
     res.render('guide.html', sheets.cache.guides[guideIndex]);
 });
+//-------------------------------------------------------------------
 
 app.get('/swamp-symphony', function(req, res) {
   res.render('symphony.html');
+});
+
+app.get('/about', function (req, res) {
+  res.render('about.html');
 });
 
 app.listen(PORT, () => console.log('Listening on port %d!', PORT));

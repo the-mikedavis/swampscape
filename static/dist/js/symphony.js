07fbbff3cc06f2fa67c742f6532d1803cb27265b
Michael Davis <mcd164@miami.edu>
2018-05-01 13:41:16 -0400

    Add the composition

--- static/dist/js/symphony.js
+++ static/dist/js/symphony.js
@@ -1,5 +1,23 @@
 var players = [];
 for (var i = 0; i < 10; i++)
   players.push(new Tone.Player({
-    url: '/dist/sounds/2.2.' + i + '.mp3'
+    url: '/dist/sounds/2.2.' + i + '.mp3',
+    loop: true
   }).toMaster());
+var composition = new Tone.Player({
+  url: '/dist/sounds/composition.mp3'
+}).toMaster();
+
+function startComposition() {
+  try {
+  composition.start();
+  } catch (e) {
+    console.log("Composition hasn't finished loading yet. Please try again.");
+  }
+}
+
+function slideOver() {
+  document.getElementById('symphony-landing').classList.add('slid');
+  document.getElementById('symphony-player').classList.add('slid');
+  composition.stop();
+}

0021d6c30874c1045a596763d82fcb4257772f19
Michael Davis <mcd164@miami.edu>
2018-05-03 09:20:42 -0400

    Fix the issue of miltiple running sass nodes

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-sass --watch sass:static/dist/css &
-nodemon -e js,html,css index.js ${1:-3030}
+sass --watch sass:static/dist/css & nodemon -e js,html,css index.js ${1:-3030} && kill $!
